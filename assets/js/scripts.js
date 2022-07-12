736d531f69e737c68bc23d7035192177ee89395b
RenePessoto <ra_pessoto@hotmail.com>
2022-07-12 00:47:31 -0300

    focus on button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,25 +6,37 @@ const movie ={
     Cover:(movieURL)
 };
 
+var addButtonEl = document.querySelector('#add-movie');
+
 function addMovie(){
 
-    const movieName = document.getElementById('name').value;
-    const movieURL = document.getElementById('url').value;
+  let movieName = document.getElementById('name').value;
+  let movieURL = document.getElementById('url').value;
 
   if ((movieURL.endsWith(".jpg") || movieURL.endsWith(".png") || movieURL.endsWith(".jpeg"))&&(movieName!== null)) {
+    
     movieList.push(new movie);
-    return movieList;
+    
+    return movieList[movie];
+  
   } else {
-    alert("URL must ends with JPG, JPEG or PNG");
-  } 
+    alert("URL must ends with JPG, JPEG or PNG and Name must be filled!");
+} 
 }
 
-document.getElementById("url").value = "";
-document.getElementById("name").value ="";
+addButtonEl.addEventListener('click',addMovie,cleanInput)
 
-function show(movieName,movieURL) {
-  var screen = "<img src=" + movieURL + " alt = "+ movieName + ">" +"<label>"+movieName+"</label>";
+function cleanInput(){
+  return (
+    document.getElementById("url").value = "",
+    document.getElementById("name").value ="")
+}
+
+function show(movieList) {
+  var screen = "<img src=" + movieURL + " alt = "+ movieName + ">" + "</br>"+ "<label>"+movieName+"</label>";
   var show = document.getElementById("show");
   show.innerHTML = show.innerHTML + screen;
+
+  
 }
 
